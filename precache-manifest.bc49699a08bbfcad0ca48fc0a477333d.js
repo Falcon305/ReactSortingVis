e40591e33ee49d6502c7ea90035d3eee87acf6dc
Hatim Razani <hrazani@e1r10p1.1337.ma>
2020-11-17 01:45:12 +0100

    Updates

---
+++ precache-manifest.bc49699a08bbfcad0ca48fc0a477333d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90d0864be5250118f5e2ea1d70fe362",
+    "url": "/ReactSortingVis/index.html"
+  },
+  {
+    "revision": "f055cc30f4adef5df470",
+    "url": "/ReactSortingVis/static/css/main.b1f85735.chunk.css"
+  },
+  {
+    "revision": "4cee6bedc07e0b8583a7",
+    "url": "/ReactSortingVis/static/js/2.05beb350.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactSortingVis/static/js/2.05beb350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f055cc30f4adef5df470",
+    "url": "/ReactSortingVis/static/js/main.09c10b02.chunk.js"
+  },
+  {
+    "revision": "8934049bb076a808abd8",
+    "url": "/ReactSortingVis/static/js/runtime-main.b43b8c7e.js"
+  }
+]);
